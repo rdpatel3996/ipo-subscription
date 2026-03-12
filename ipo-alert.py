ec739e1135c1e288d58016487244cfd9c5abd969
Raj Dipakkumar Patel <rajdipakkumar.patel@factset.com>
2026-03-12 15:11:26 +0530

    fix(chrome): fixing chrome for github action

--- ipo-alert.py
+++ ipo-alert.py
@@ -66,7 +66,10 @@ def start_browser():
     log("Starting Chrome browser")
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
